4c8f133c52389a6260ab50a370778494291bc03c
Kalvin Garcia <plutea.leaf@gmail.com>
2023-09-13 14:28:28 -0500

    tested out axios and the authentication in the api

--- frontend/src/pages/recipe.js
+++ frontend/src/pages/recipe.js
@@ -1,9 +1,26 @@
 import * as React from "react"
+import axios from 'axios';
+
+import auth from '../../../sessions/api/auth.json';
+
+let url = "http://127.0.0.1:9000/contentmanager/get-recipe-by-name/Elote/"
 
 const RecipePage = () => {
+  const [recipe_data, set_recipe_data] = React.useState();
+
+  React.useEffect(() => {
+    axios.get(url, {
+      headers: {
+        'Authorization': `Token ${auth.token}`
+      }
+    }).then(response => {
+      set_recipe_data(response.data)
+    })
+  });
+
   return (
-    <main></main>
-  )
+    <main>{JSON.stringify(recipe_data)}</main>
+  );
 }
 
 export default RecipePage
