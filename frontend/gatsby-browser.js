602d57def08bc919b54eaaf8ec240b789f35aac7
Kalvin Garcia <plutea.leaf@gmail.com>
2023-09-26 19:30:09 -0500

    removed the import from the gatsby-browser file

--- frontend/gatsby-browser.js
+++ frontend/gatsby-browser.js
@@ -1,5 +1,1 @@
 import './src/styles/global.scss';
-import './src/styles/recipelibrary.scss';
-import './src/styles/allrecipecarousel.scss';
-import './src/styles/filteraccordion.scss';
-import './src/styles/header.scss';
