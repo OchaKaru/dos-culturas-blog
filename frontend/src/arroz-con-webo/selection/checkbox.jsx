080812d06f825cd8f8badd1d518f6586e3c0fa35
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-06 18:25:07 -0500

    removed the reference and ripple effect from the checkbox

--- frontend/src/arroz-con-webo/selection/checkbox.jsx
+++ frontend/src/arroz-con-webo/selection/checkbox.jsx
@@ -17,35 +17,9 @@ function Checkbox(props) {
 
     const role = "primary";
 
-    let reference = React.useRef(null);
-   
-    // this is an attempt at making a ripple effect for the wrapper, but i need to figure out how to keep the animation playing even on mouse up
-    // React.useEffect(() => {
-    //     if(reference.current)
-    //         reference.current.addEventListener('mousedown', (event) => {
-    //             const checkbox = event.currentTarget;
-    //             const circle = document.createElement("span");
-    //             const radius = checkbox.clientWidth > checkbox.clientHeight? checkbox.clientWidth / 2 : checkbox.clientHeight / 2;
-    //             circle.style.width = circle.style.height = `${radius * 2}px`;
-    //             circle.style.left = `${event.clientX - checkbox.offsetLeft - radius}px`;
-    //             circle.style.top = `${event.clientY - checkbox.offsetTop - radius}px`;
-    //             // circle.style.zIndex = '-1';
-    //             circle.classList.add(`arroz-checkbox-ripple`);
-
-    //             checkbox.appendChild(circle);
-    //         });
-    //         reference.current.addEventListener('mouseup', (event) => {
-    //             const checkbox = event.currentTarget;
-
-    //             const ripple = checkbox.getElementsByClassName(`arroz-checkbox-ripple`)[0];
-    //             if(ripple)
-    //                 ripple.remove();
-    //         });
-    // }, [])
-
     return (
         <label htmlFor={label} className='arroz-checkbox'>
-            <div ref={reference} className='arroz-checkbox-checkbox' >
+            <div className='arroz-checkbox-checkbox' >
                 <input id={label} type="checkbox" name={label} checked={checked} onClick={() => set_checked(!checked)} onChange={props.onToggle}/>
             </div>
             <span className='arroz-checkbox-label'>
