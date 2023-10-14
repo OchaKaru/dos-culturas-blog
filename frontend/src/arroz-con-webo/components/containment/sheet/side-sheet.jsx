36a0f147beee1996554b9cdc6122ae8054df9b33
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-13 19:11:26 -0500

    finished moving the styles into the component and changed to CSSTransition

--- frontend/src/arroz-con-webo/components/containment/sheet/side-sheet.jsx
+++ frontend/src/arroz-con-webo/components/containment/sheet/side-sheet.jsx
@@ -3,8 +3,8 @@ import {CSSTransition} from 'react-transition-group';
 
 // arroz imports
 import {InvalidRoleError, InvalidContainerError} from '../../../error';
-import {Manager, Scheme, Typography} from '../../../styles';
-import {useWindowDimensions, clamp} from '../../../util';
+import {ThemeContext, Typography} from '../../../styles';
+import {clamp, useWindowDimensions, useCSSClass} from '../../../util';
 import {ContainerContext, valid_container, valid_role} from '../container-context';
 
 /**
@@ -12,49 +12,48 @@ import {ContainerContext, valid_container, valid_role} from '../container-contex
  * even other containers.
  * 
  * @param {boolean} open (required) Specifies whether to open the side-sheet. This allows complete customization over which button opens the sheet.
- * @param {string} role (optional) Can values of "primary", "secondary", "tertiary", "error", and "neutral". Defaults to "neutral";
+ * @param {string} role (optional) Can values of "primary", "secondary", "tertiary", "error", and "neutral". Defaults to "neutral".
  * @param {string} containerType (optional) Can use values of "container_[level]". Defaults to "container_lowest".
- * @param {boolean} modal
+ * @param {boolean} animate (optional) Specifies whether the side sheet should animate open and close. Defaults to true.
+ * @param {boolean} modal (optional) Specifies whether the side sheet should be modal.
  */
-function SideSheet(props) {
-    if(props.role && !valid_role(props.role))
+function SideSheet({className, children, open, role = "neutral", containerType = "container_lowest", animate = true, modal}) {
+    if(role && !valid_role(role))
         throw new InvalidRoleError();
-    const role = props.role?? 'neutral';
-    if(props.containerType && !valid_container(role, props.containerType))
+    if(containerType && !valid_container(role, containerType))
         throw new InvalidContainerError();
-    const container_type = props.containerType?? 'container_lowest';
 
     const context = React.useContext(ContainerContext);
+    const {Scheme} = React.useContext(ThemeContext);
 
     // Defining the animation duration and the transition information
     const ANIMATION_DURATION = 300;
 
-    const {'width': window_width} = useWindowDimensions();
-    const [side_sheet_width, set_sheet_width] = React.useState();  
+    const {'width': window_width} = useWindowDimensions(); 
+    const [class_name, set_style] = useCSSClass();
     React.useEffect(() => {
-        const sheet_size = width => clamp(-0.0332 * width + 88.7448, 25, 75);
-        set_sheet_width(sheet_size(window_width) / 100 * window_width);
+        const side_sheet_width = clamp(-0.0332 * window_width + 88.7448, 25, 75) / 100 * window_width;
 
-        Manager.style_sheet("side_sheet", `
-            .arroz-side-sheet {
+        set_style(`
+            .${class_name} {
                 // color
-                background-color: ${(context.role === role && context.container_type === container_type)? "transparent" : Scheme[role][container_type]};
-                color: ${Scheme[role].on_container}
+                background-color: ${(context.role === role && context.container_type === containerType)? "transparent" : Scheme[role][containerType]};
+                color: ${(context.role === role && context.container_type === containerType)? Scheme[context.role].on_container : Scheme[role][containerType]}
 
                 // structure
                 height: 100%;
                 align-self: flex-start;
                 overflow-x: hidden;
 
-                position: ${props.modal? "absolute" : "relative"};
-                ${props.modal?
+                position: ${modal? "absolute" : "relative"};
+                ${modal?
                     `border-radius: 0 ${Typography.font_size}${Typography.unit} ${Typography.font_size}${Typography.unit} 0;` :
                     `border-right: ${0.1 * Typography.font_size}${Typography.unit};`
                 }
                 z-index: 100;
             }
 
-            .arroz-modal-shim {
+            .${class_name}-shim {
                 // color
                 background-color: ${Scheme.neutral.on_container};
                 opacity: 50%;
@@ -65,37 +64,48 @@ function SideSheet(props) {
                 position: absolute;
                 z-index: 100;
 
-                display: ${props.modal? "block" : "none"}
+                display: ${modal? "block" : "none"}
             }
 
             // side sheet animation
-            .arroz-side-sheet-enter {
+            .side-sheet-open {
                 width: 0;
             }
-            .arroz-side-sheet-enter-active {
+            .side-sheet-open-active {
                 width: ${side_sheet_width}px;
                 transition: width ${ANIMATION_DURATION}ms ease;
             }
 
-            .arroz-side-sheet-exit {
+            .side-sheet-close {
                 width: ${side_sheet_width}px;
             }
-            .arroz-side-sheet-exit-active {
+            .side-sheet-close-active {
                 width: 0;
                 transition: width ${ANIMATION_DURATION}ms ease;
             }
         `)
-    }, [window_width, Scheme])
+    }, [window_width, Scheme, class_name, containerType, role, set_style, context.container_type, context.role, modal]);
+
+    let reference = React.useRef(null);
 
     return (
-        <ContainerContext.Provider value={{"role": role, "container_type": container_type}}>
-            <div className="arroz-modal-shim" />
+        <ContainerContext.Provider value={{"role": role, "container_type": containerType}}>
+            <div className={`${class_name}-shim`} />
             <CSSTransition
+                in={open}
                 nodeRef={reference}
-                in={props.open}
+                addEndListener={(done) => {
+                    reference.current.addEventListener("transitionend", done, false);
+                }}
+                classNames={animate? {
+                    "enter": "side-sheet-open",
+                    "enterActive": "side-sheet-open-active",
+                    "exit": "side-sheet-close",
+                    "exitActive": "side-sheet-close-active"
+                } : "undefined"}
             >
-                <div ref={reference} className={"arroz-side-sheet " + props.className?? ""}>
-                    {props.children}
+                <div ref={reference} className={`${class_name} ${className?? ""}`}>
+                    {children}
                 </div>
             </CSSTransition>
         </ContainerContext.Provider>
