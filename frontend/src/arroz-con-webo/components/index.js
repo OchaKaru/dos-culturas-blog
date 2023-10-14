ee9f894d28f5c28f3d8f55a49fbce3bb3177526b
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-13 19:08:54 -0500

    added exports for all the components

--- frontend/src/arroz-con-webo/components/index.js
+++ frontend/src/arroz-con-webo/components/index.js
@@ -2,8 +2,23 @@
  * 
  */
 
-export * from './action';
-export * from './communication';
-export * from './containment';
-export * from './selection';
-export * from './specialized';
+// Action Imports
+export {default as ElevatedButton} from './action/common-button/elevated-button';
+export {default as FilledButton} from './action/common-button/filled-button';
+export {default as OutlinedButton} from './action/common-button/outlined-button';
+export {default as TextButton} from './action/common-button/text-button';
+export {default as TonalButton} from './action/common-button/tonal-button';
+
+// export * from './communication';
+
+export {default as ElevatedCard} from './containment/card/elevated-card';
+export {default as FilledCard} from './containment/card/filled-card';
+export {default as OutlinedCard} from './containment/card/outlined-card';
+export {default as Slideshow} from './containment/presentation/slideshow';
+export {default as SideSheet} from './containment/sheet/side-sheet';
+export * from './containment/container-context';
+export {default as Submenu} from './containment/submenu';
+
+export {default as Checkbox} from './selection/checkbox';
+
+// export * from './specialized';
