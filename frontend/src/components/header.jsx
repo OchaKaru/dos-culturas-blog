b459e7e83e69643ccc7d003ae6e8e16e560aebe4
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-23 18:57:29 -0500

    moving and simplifying the website components

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import {navigate} from "gatsby";
+import {FilledButton, Subheading, TextButton} from "../arroz-con-webo";
+
+import logo from "../images/temp_logo.svg";
+
+export default function Header() {
+    const [active, set_active] = React.useState('home');
+    const [nav_list, update_nav_list] = React.useState([]);
+
+    React.useEffect(() => {
+        const go = page => {
+            set_active(page);
+            navigate(`/${page}/`);
+        }
+
+        let nav_list = [];
+        if(active === 'home')
+            nav_list.push(<FilledButton role="primary" pill>Home</FilledButton>);
+        else
+            nav_list.push(<TextButton role="primary" pill onClick={() => go("home")}>Home</TextButton>);
+
+        if(active === 'library')
+            nav_list.push(<FilledButton role="primary" pill>Recipes</FilledButton>);
+        else
+            nav_list.push(<TextButton role="primary" pill onClick={() => go("library")}>Recipes</TextButton>);
+
+        if(active === 'about')
+            nav_list.push(<FilledButton role="primary" pill>About</FilledButton>);
+        else
+            nav_list.push(<TextButton role="primary" pill onClick={() => go("about")}>About</TextButton>);
+        
+        update_nav_list(nav_list);
+    }, [active]);
+
+    return (
+        <div className="header">
+            <div className="header-logo">
+                <img src={logo} alt="logo" />
+                <Subheading>Dos Culturas</Subheading>
+            </div>
+            <ul className="nav-links">
+                {nav_list}
+            </ul>
+        </div>
+    );
+}
---
+++ frontend/src/components/recipe-carousel.jsx
@@ -0,0 +1,47 @@
+import * as React from 'react';
+import {Slideshow, Pane} from '../arroz-con-webo';
+
+import RecipeCard from './recipe-card';
+
+export default function RecipeCarousel({data}) {
+    const MAX_RECIPES_ON_PAGE = 6;
+    const [page_list, set_page_list] = React.useState();
+
+    // receives a list of objects containing: image, name, description
+    function pagify_data(data) {
+        let pages = [];
+
+        function cardify_data(data) {
+            let cards = [];
+    
+            if(data)
+                for(let i = 0; i < data.length; i++)
+                    cards.push(<RecipeCard key={i} index={i} data={data[i]} />);
+            return cards;
+        }
+
+        if(data)
+            for(let i = 0; i < data.length; i += MAX_RECIPES_ON_PAGE) {
+                const chunk = data.slice(i, i + MAX_RECIPES_ON_PAGE);
+                pages.push(
+                    <div className="all-recipe-slide" style={{"width": "1200px"}}>
+                        {cardify_data(chunk)}
+                    </div>
+                );
+            }
+
+        return pages;
+    }
+
+    React.useEffect(() => {
+        set_page_list(pagify_data(data));
+    }, [data])
+
+    return (
+        <Pane className='all-recipe-carousel' containerType="container" rounded>
+            <Slideshow enterStyle="fade-in-up" exitStyle="fade-out-against">
+                {page_list}
+            </Slideshow>
+        </Pane>
+    );
+}
