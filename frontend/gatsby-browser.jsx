a9580d943a48a5a545927422aee9a12eb074a3b4
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-23 18:58:40 -0500

    added a header and footer to the wrapper component

--- frontend/gatsby-browser.jsx
+++ frontend/gatsby-browser.jsx
@@ -2,7 +2,15 @@ const React = require("react");
 const Arroz = require('./src/arroz-con-webo');
 
 require('./src/styles/global.scss');
+const Header = require('./src/components/header').default;
+// require('./src/components/footer');
 
 exports.wrapPageElement = ({element, props}) => {
-    return <Arroz.Root {...props}>{element}</Arroz.Root>
+    return (
+        <Arroz.Root {...props}>
+            <Header />
+            {element}
+            {/* <Footer /> */}
+        </Arroz.Root>
+    );
 }
