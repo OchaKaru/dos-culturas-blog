027bfa800b1a5f9fea0bf270b04c1afe6362e135
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-23 19:00:47 -0500

    fixed the tansition animation

--- frontend/src/arroz-con-webo/components/presentation/slideshow.jsx
+++ frontend/src/arroz-con-webo/components/presentation/slideshow.jsx
@@ -108,15 +108,15 @@ export default function Slideshow({className, enterStyle = "fade-in", exitStyle 
         exitBeforeEnter: mode === "out-in",
         from: {
             opacity: 0,
-            transform: "translateY(-100%)"
+            transform: "translate(0%, 100%)"
         },
         enter: {
             opacity: 1,
-            transform: "translateY(0%)"
+            transform: "translate(0%, 0%)"
         },
         leave: {
-            opacity: 1,
-            transform: `translateX(${direction === 'left'? "100%" : "-100%"})`
+            opacity: 0,
+            transform: `translate(${direction === 'left'? "100%" : "-100%"}, 0%)`
         }
     });
 
