cae63a8e369720634bc3c2bb7fa157cf2cd04ef4
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-24 20:28:21 -0500

    added the component structure to about page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -1,8 +1,24 @@
 import * as React from "react";
+import {Form, OutlinedTextBox, OutlinedTextArea, Headline, Body, Image, FilledCard, Subheading} from "../arroz-con-webo";
 
 const AboutPage = () => {
   return (
     <main>
+      <Headline>About</Headline>
+      <div>
+        <div>
+          <Image />
+          <Body></Body>
+        </div>
+        <FilledCard>
+          <Subheading>Contact Us</Subheading>
+          <Form>
+            <OutlinedTextBox>example@email.com</OutlinedTextBox>
+            <OutlinedTextBox>Jose Tamal</OutlinedTextBox>
+            <OutlinedTextArea />
+          </Form>
+        </FilledCard>
+      </div>
     </main>
   );
 }
