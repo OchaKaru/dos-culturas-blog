18b0784dcf078889fee14425ed9008116970caf4
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-30 17:59:05 -0500

    added reference to the featured recipe api

--- frontend/src/components/weekly-recipe-card.jsx
+++ frontend/src/components/weekly-recipe-card.jsx
@@ -1,18 +1,28 @@
 import * as React from 'react';
+import {Link} from 'gatsby';
 import {FilledCard, Image, Title, Subheading, Label, Body} from '../arroz-con-webo';
 
-export default function WeeklyRecipeCard({name, culture, description}) {
+import '../styles/home/weekly-recipe-card.scss';
+
+export default function WeeklyRecipeCard({data}) {
+    const [image, set_image] = React.useState("");
+    React.useEffect(() => {
+        (async () => {
+            set_image((await import(`../images/${data?.image}`)).default);
+        })();
+    }, [data])
+
     return (
-        <FilledCard role="primary" containerType="container" rounded interactable>
-            <div>
-                <Image />
-                <div>
+        <Link to="/recipe/" state={{'recipe_clicked': data?.pk}}>
+            <FilledCard className="weekly-recipe-card" role="primary" containerType="container" rounded interactable>
+                <Image source={image} />
+                <div className="weekly-recipe-info">
                     <Title>Weekly Recipe!</Title>
-                    <Subheading>{name}</Subheading>
-                    <Label>{culture}</Label>
-                    <Body>{description}</Body>
+                    <Subheading>{data?.name}</Subheading>
+                    <Label role="primary" pill>{data?.culture}</Label>
+                    <Body>{data?.desc}</Body>
                 </div>
-            </div>
-        </FilledCard>
+            </FilledCard>
+        </Link>
     );
 }
