7b2174905e182705bf6f7f5641b7947d2e760445
Kalvin Garcia <plutea.leaf@gmail.com>
2023-10-30 17:54:56 -0500

    styled the featured recipe component

--- frontend/src/components/featured-recipes.jsx
+++ frontend/src/components/featured-recipes.jsx
@@ -3,15 +3,21 @@ import * as React from 'react';
 import RecipeCard from './recipe-card';
 import {Title} from '../arroz-con-webo';
 
+import '../styles/home/featured-recipes.scss';
+
 export default function FeaturedRecipes({data}) {
+    React.useEffect(() => {
+        console.log(data);
+    })
+
     return (
-        <>
+        <div className='featured-recipes'>
             <Title>Features Recipes</Title>
-            <div>
+            <div className='card-container'>
                 <RecipeCard data={data[0]} />
                 <RecipeCard data={data[1]} />
                 <RecipeCard data={data[2]} />
             </div>
-        </>
+        </div>
     );
 }
