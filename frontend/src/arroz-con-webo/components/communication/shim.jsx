e3b6b0ab08ddf5a4736a18e57fbce27102e5c08d
Kalvin Garcia <plutea.leaf@gmail.com>
2023-11-06 18:05:03 -0600

    added the shim as a component

---
+++ frontend/src/arroz-con-webo/components/communication/shim.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {createUseStyles} from 'react-jss';
+
+const useStyles = createUseStyles(({theme}) => ({
+    "arroz-shim": {
+        backgroundColor: theme.scheme.neutral.shadow,
+        opacity: "50%",
+        height: "100%",
+        width: "100%",
+        position: "absolute",
+        zIndex: 100,
+        display: ({show}) => show? "block" : "none"
+    }
+}));
+
+export default function Shim({show = false}) {
+    const classes = useStyles({show});
+    return (
+        <div className={`${classes['arroz-shim']}`} />
+    );
+}
