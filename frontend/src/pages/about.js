6de945fe89cb31850f8ae993e6081daef139c16b
Kalvin Garcia <plutea.leaf@gmail.com>
2023-11-08 15:37:13 -0600

    added styles to the about page

--- frontend/src/pages/about.js
+++ frontend/src/pages/about.js
@@ -2,15 +2,15 @@ import * as React from "react";
 import {Form, OutlinedTextBox, OutlinedTextArea, Headline, Body, Image, FilledCard, Subheading, FilledButton} from "../arroz-con-webo";
 
 import image from '../images/tamales_broll.png';
+import '../styles/about/about-page.scss';
 
 export default function AboutPage() {
   return (
-    <main>
-      <Headline>About</Headline>
-      <div>
-        <div>
-          <Image source={image} />
-          <Body>
+    <main className="about-page">
+      <div className="about-bio">
+        <Headline>About</Headline>
+        <Image className="about-image" source={image} />
+        <Body>
           Welcome to 'Dos Culturas,' a culinary journey into the heart of authentic and delicious
           recipes from Mexico and Puerto Rico. Our website is a vibrant tapestry of diverse and time-honored cuisines,
           celebrating the rich cultural heritage of each dish.<br />
@@ -23,17 +23,16 @@ export default function AboutPage() {
           Whether you're an aspiring chef, a home cook, or simply an enthusiast of hispanic cuisine, 'Dos Culturas'
           is your gateway to savor the world's most cherished culinary treasures. Join us on a journey of taste and tradition,
           one recipe at a time. Buen provecho!<br />
-          </Body>
-        </div>
-        <FilledCard role="secondary">
-          <Subheading>Contact Us</Subheading>
-          <Form>
-            <OutlinedTextBox label="Your email" rounded>example@email.com</OutlinedTextBox>
-            <OutlinedTextBox label="Your name" rounded>Jose Tamal</OutlinedTextBox>
-            <FilledButton role="primary" pill type="submit">Send!</FilledButton>
-          </Form>
-        </FilledCard>
+        </Body>
       </div>
+      <FilledCard className="contact-us" role="secondary">
+        <Subheading>Contact Us</Subheading>
+        <Form>
+          <OutlinedTextBox label="Your email" rounded>example@email.com</OutlinedTextBox>
+          <OutlinedTextBox label="Your name" rounded>Jose Tamal</OutlinedTextBox>
+          <FilledButton role="primary" pill type="submit">Send!</FilledButton>
+        </Form>
+      </FilledCard>
     </main>
   );
 }
