e0ddb80f911e0e588280a6baed5abae8a211c863
Kalvin Garcia <plutea.leaf@gmail.com>
2023-11-13 10:45:23 -0600

    deploying next site

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,16 +1,18 @@
 const path = require('path');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: isProd? '/dos-culturas-blog' : '',
+  output: 'export',
   reactStrictMode: true,
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
   images: {
-    loader: 'akamai',
-    path: '',
+    unoptimized: true,
   },
-  assetPrefix: './',
 };
 
 module.exports = nextConfig;
